95e12c75f7be04ead1b2457363ea9371a6ef6b8f
Vladislav <vladislav.tezev@gmail.com>
2025-06-03 04:35:52 +0300

    Add entity for exhaustion-order
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

---
+++ backend/src/main/java/ru/itmo/sem/backend/repository/ExhaustionOrderRepository.java
@@ -0,0 +1,9 @@
+package ru.itmo.sem.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itmo.sem.backend.model.ExhaustionOrder;
+
+import java.util.UUID;
+
+public interface ExhaustionOrderRepository extends JpaRepository<ExhaustionOrder, UUID> {
+}

50095f7dda59bf301462bf7b63bba1b25e011cca
Vladislav <vladislav.tezev@gmail.com>
2025-06-04 16:44:43 +0300

    Add initializations
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

---
+++ backend/src/main/java/ru/itmo/sem/backend/config/MagicInitializer.java
@@ -0,0 +1,33 @@
+package ru.itmo.sem.backend.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import ru.itmo.sem.backend.model.entity.Magic;
+import ru.itmo.sem.backend.repository.MagicRepository;
+
+@Component
+public class MagicInitializer implements ApplicationRunner {
+
+    protected final MagicRepository magicRepository;
+
+    public MagicInitializer(MagicRepository magicRepository) {
+        this.magicRepository = magicRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        int volume = 10;
+        for (int i = 1; i <= 5; i++) {
+            magicRepository.save(
+                    new Magic(
+                            (long) i,
+                            volume
+                    )
+            );
+
+            volume += 4;
+        }
+    }
+
+}
--- backend/src/main/java/ru/itmo/sem/backend/config/RoleInitializer.java
+++ backend/src/main/java/ru/itmo/sem/backend/config/RoleInitializer.java
@@ -3,7 +3,7 @@ package ru.itmo.sem.backend.config;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import ru.itmo.sem.backend.model.Role;
+import ru.itmo.sem.backend.model.user.Role;
 import ru.itmo.sem.backend.model.enums.RoleType;
 import ru.itmo.sem.backend.repository.RoleRepository;
 
